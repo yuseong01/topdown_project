1e1fa6ac12b7fd88e250a7b7ec5789ab295a21e8
JangYuseong <dog2655@naver.com>
2025-05-05 21:15:02 +0900

    feat: 플레이어 이동, 타일맵 구성

---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertial = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertial).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if(lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+    
+}
